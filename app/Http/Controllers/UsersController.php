c67fa2eaf7deaa54c0bd879be2c279a7a04d3808
gersar08 <nagerman26@gmail.com>
2024-05-14 17:49:10 -0600

    Fix the return parameter from index function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::all();
-        response()->json([$users], 200);
+        return response()->json([$users], 200);
     }
 
     public function store(Request $request)
